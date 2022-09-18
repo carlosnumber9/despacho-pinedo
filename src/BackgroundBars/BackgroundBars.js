04e41902910f38b54d6f0445dd0059aaa1bf58d9
Carlos Pinedo <carlos.pinedo@devo.com>
2022-09-18 02:36:08 +0200

    fix: Solved bars disappearing bug

--- src/BackgroundBars/BackgroundBars.js
+++ src/BackgroundBars/BackgroundBars.js
@@ -2,8 +2,8 @@ import './bg-bars.css';
 
 export const BackgroundBars = () => (
   <div id="bg-bar-wrapper">
-    <div id="bg-bar-1" className="bg-bar"></div>
-    <div id="bg-bar-2" className="bg-bar"></div>
-    <div id="bg-bar-3" className="bg-bar"></div>
+    <div id="bg-bar-1" className="bg-bar" />
+    <div id="bg-bar-2" className="bg-bar" />
+    <div id="bg-bar-3" className="bg-bar" />
   </div>
 );
